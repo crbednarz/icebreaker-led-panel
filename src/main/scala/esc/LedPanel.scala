b1d8077275f1cea534d47901786054fa638afc25
cr.bednarz@gmail.com <cr.bednarz@gmail.com>
2020-05-22 23:38:44 -0700

    RGB lighting works!

--- src/main/scala/esc/LedPanel.scala
+++ src/main/scala/esc/LedPanel.scala
@@ -1,6 +1,9 @@
 package esc
 
 import spinal.core._
+import spinal.lib.{Reverse, slave}
+import spinal.lib.fsm._
+import spinal.lib.graphic.{Rgb, RgbConfig}
 
 case class LedPanel() extends Bundle {
   val address = out UInt(5 bits)
@@ -9,4 +12,123 @@ case class LedPanel() extends Bundle {
   val clock = out Bool
   val latch = out Bool
   val blank = out Bool
+}
+
+case class PwmPainter() extends Component {
+  val io = new Bundle {
+    val frameCount = in UInt(8 bits)
+    val inColor = in(Rgb(RgbConfig(8, 8, 8)))
+    val outColor = out(RgbShift())
+  }
+  io.outColor.r := (io.inColor.r > Reverse(io.frameCount))
+  io.outColor.g := (io.inColor.g > Reverse(io.frameCount))
+  io.outColor.b := (io.inColor.b > Reverse(io.frameCount))
+}
+
+case class LedPanelController() extends Component {
+  val io = new Bundle {
+    val led = LedPanel()
+    val color = slave Stream(Rgb(RgbConfig(8, 8, 8)))
+  }
+
+  val led = Reg(LedPanel())
+  led.address init(0)
+  led.top.r init(False)
+  led.top.g init(False)
+  led.top.b init(False)
+  led.bottom.r init(False)
+  led.bottom.g init(False)
+  led.bottom.b init(False)
+  led.latch init(True)
+  led.blank init(True)
+  io.led.address <> led.address
+  io.led.top <> led.top
+  io.led.bottom <> led.bottom
+  io.led.latch <> led.latch
+  io.led.blank <> led.blank
+
+  val frame = Mem(Rgb(RgbConfig(8, 8, 8)), 32 * 32)
+
+  val row = Reg(UInt(5 bits)) init(0)
+  val column = Reg(UInt(7 bits))
+  val clockEnabled = Reg(Bool) init(False)
+  val frameCount = Reg(UInt(32 bits)) init(0)
+  val readAddress = UInt(10 bits)
+  val writeAddress = Reg(UInt(10 bits)) init(0)
+
+  val topColor = Reg(Rgb(RgbConfig(8, 8, 8)))
+  val topPainter = PwmPainter()
+  topPainter.io.frameCount <> frameCount(7 downto 0)
+  led.top <> topPainter.io.outColor
+  topPainter.io.inColor <> topColor
+
+  val bottomColor = Reg(Rgb(RgbConfig(8, 8, 8)))
+  val bottomPainter = PwmPainter()
+  bottomPainter.io.frameCount <> frameCount(7 downto 0)
+  led.bottom <> bottomPainter.io.outColor
+  bottomPainter.io.inColor <> bottomColor
+
+  readAddress := (row >> 1).resize(5) @@ (column >> 1).resize(5)
+  topColor := frame(readAddress)
+  bottomColor := frame(readAddress + 512)
+
+  when (io.color.valid) {
+    frame(writeAddress) := io.color.payload
+    writeAddress := writeAddress + 1
+  }
+
+  val ddr = DDR()
+  ddr.io.clockIn <> ClockDomain.current.readClockWire
+  ddr.io.clockEnable <> clockEnabled
+  io.led.clock <> ddr.io.clockOut
+
+  val stateMachine = new StateMachine {
+    val blankState = new State with EntryPoint
+    val latchState = new State
+    val unlatchState = new State
+    val unblankState = new State
+    val shiftState = new State
+
+    blankState.whenIsActive {
+      led.blank := True
+      led.address := row
+      goto(latchState)
+    }
+
+    latchState.whenIsActive {
+      led.latch := True
+      goto(unlatchState)
+    }
+
+    unlatchState.whenIsActive {
+      led.latch := False
+      goto(unblankState)
+    }
+
+    unblankState.whenIsActive {
+      led.blank := False
+      row := row + 1
+      when (row === 31) {
+        frameCount := frameCount + 1
+      }
+      goto(shiftState)
+    }
+
+    shiftState
+      .onEntry {
+        column := 0
+        readAddress := 0
+        clockEnabled := True
+      }
+      .whenIsActive {
+        column := column + 1
+        when (column(6)) {
+          goto(blankState)
+        }
+      }
+      .onExit {
+        clockEnabled := False
+
+      }
+  }
 }
--- src/main/scala/esc/Top.scala
+++ src/main/scala/esc/Top.scala
@@ -3,6 +3,7 @@ package esc
 import spinal.core._
 import spinal.lib.bus.amba3.apb.{Apb3, Apb3Config}
 import spinal.lib.com.uart._
+import spinal.lib.fsm.{EntryPoint, StateMachine}
 import spinal.lib.graphic.{Rgb, RgbConfig}
 import spinal.lib.{CounterFreeRun, Stream, master}
 
@@ -27,14 +28,44 @@ class Top extends Component {
   }
 
   val core = new ClockingArea(clockControl.domain) {
+    io.leds := 0
+
+
     val uartCtrl = new UartCtrl()
-    uartCtrl.io.config.setClockDivider(9600 Hz)
-    uartCtrl.io.config.frame.dataLength := 7  //8 bits
+    uartCtrl.io.config.setClockDivider(921600 Hz)
+    uartCtrl.io.config.frame.dataLength := 7
     uartCtrl.io.config.frame.parity := UartParityType.NONE
     uartCtrl.io.config.frame.stop := UartStopType.ONE
     uartCtrl.io.uart <> io.uart
     uartCtrl.io.writeBreak := False
-    io.leds := uartCtrl.io.read.toFlow.toReg()(4 downto 0)
+
+
+    val test = Reg(Rgb(RgbConfig(8, 8, 8)))
+    test.r init(0)
+    test.g init(0)
+    test.b init(0)
+
+    val counter = Reg(UInt(2 bits)) init(0)
+    val data = Reg(Vec(Bits(8 bits), 3))
+    val colorStream = Stream(Rgb(RgbConfig(8, 8, 8)))
+
+    when (uartCtrl.io.read.valid) {
+      data(counter) := uartCtrl.io.read.payload
+      counter := counter + 1
+    }
+
+    when (counter === 3) {
+      counter := 0
+      colorStream.payload.r := U(data(0))
+      colorStream.payload.g := U(data(1))
+      colorStream.payload.b := U(data(2))
+      colorStream.valid := True
+    } otherwise {
+      colorStream.payload.r := 0
+      colorStream.payload.g := 0
+      colorStream.payload.b := 0
+      colorStream.valid := False
+    }
 
     val write = Stream(Bits(8 bits))
     write.valid := CounterFreeRun(2000).willOverflow
@@ -43,6 +74,7 @@ class Top extends Component {
 
     val ctrl = LedPanelController()
     ctrl.io.led <> io.led
+    colorStream >> ctrl.io.color
   }
 
 }
